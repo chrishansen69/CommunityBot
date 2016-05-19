fe745b25cf3544b7bfe4633193e3e5097c7edfaa
chrishansen69 <cadrekucra@gmx.com>
2016-05-19 16:24:07 -0300

    remove win32ole

--- compat.js
+++ compat.js
@@ -1,6 +1,8 @@
 // Battler compatibility polyfills
 
-var win32ole = require('win32ole');
+//var win32ole = require('win32ole');
 global.w32Factory = function(s) {
-	return win32ole.client.Dispatch(s);
+	console.log("Missing polyfill!!!");
+	return null;
+	//return win32ole.client.Dispatch(s);
 }
