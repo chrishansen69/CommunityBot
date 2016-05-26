076261c9c590b503d049215f25bee0b48a44eaae
chrishansen69 <cadrekucra@gmx.com>
2016-05-26 00:21:29 -0300

    fun2 - add missing semicolons

--- plugins/fun2/index.js
+++ plugins/fun2/index.js
@@ -11,14 +11,14 @@ Commands.gif = {
   description: "I'll search Giphy for a gif matching your tags.",
   synonyms: ['giphy'],
   fn: function (msg, suffix) {
-    var tags = suffix.split(' ')
+    var tags = suffix.split(' ');
     Giphy.get_gif(tags, function (id) {
       if (typeof id !== 'undefined') {
-        msg.reply('http://media.giphy.com/media/' + id + '/giphy.gif [Tags: ' + tags + ']')
+        msg.reply('http://media.giphy.com/media/' + id + '/giphy.gif [Tags: ' + tags + ']');
       } else {
-        msg.reply('Sorry! Invalid tags, try something else. For example something that exists [Tags: ' + tags + ']')
+        msg.reply('Sorry! Invalid tags, try something else. For example something that exists [Tags: ' + tags + ']');
       }
-    })
+    });
   }
 }
 
@@ -26,12 +26,12 @@ Commands.fortunecow = {
   description: "I'll get a random fortunecow!",
   module: 'fun',
   fn: function (msg) {
-    unirest.get('https://thibaultcha-fortunecow-v1.p.mashape.com/random')
-      .header('X-Mashape-Key', config.api_keys.mashape)
-      .header('Accept', 'text/plain')
+    unirest.get('https://thibaultcha-fortunecow-v1.p.mashape.com/random');
+      .header('X-Mashape-Key', config.api_keys.mashape);
+      .header('Accept', 'text/plain');
       .end(function (result) {
-        msg.reply('```' + result.body + '```')
-      })
+        msg.reply('```' + result.body + '```');
+      });
   }
 }
 
@@ -39,17 +39,17 @@ Commands.randomcat = {
   description: "I'll get a random cat image for you!",
   module: 'fun',
   fn: function (msg) {
-    unirest.get('https://nijikokun-random-cats.p.mashape.com/random')
-      .header('X-Mashape-Key', config.api_keys.mashape)
-      .header('Accept', 'application/json')
+    unirest.get('https://nijikokun-random-cats.p.mashape.com/random');
+      .header('X-Mashape-Key', config.api_keys.mashape);
+      .header('Accept', 'application/json');
       .end(function (result) {
         try {
-          msg.reply(result.body.source)
+          msg.reply(result.body.source);
         } catch (e) {
-          Logger.error(e)
-          msg.reply('Something went wrong, try again later.')
+          Logger.error(e);
+          msg.reply('Something went wrong, try again later.');
         }
-      })
+      });
   }
 }
 
@@ -58,11 +58,11 @@ Commands.leetspeak = {
   synonyms: ['leetspeek', 'leetspeach'],
   fn: function (msg, suffix) {
     if (suffix.length > 0) {
-      var leetspeak = require('leetspeak')
-      var thing = leetspeak(suffix)
-      msg.reply(thing)
+      var leetspeak = require('leetspeak');
+      var thing = leetspeak(suffix);
+      msg.reply(thing);
     } else {
-      msg.reply('*You need to type something to encode your message into l337sp3@K!*')
+      msg.reply('*You need to type something to encode your message into l337sp3@K!*');
     }
   }
 }
@@ -72,47 +72,47 @@ Commands.stroke = {
   fn: function (msg, suffix) {
     var name
     if (suffix) {
-      name = suffix.split('"')
+      name = suffix.split('"');
       if (name.length === 1) {
         name = ['', name]
       }
     } else {
       name = ['Andrei', 'Zbikowski'] // I'm not sorry b1nzy <3
     }
-    var request = require('request')
+    var request = require('request');
     request('http://api.icndb.com/jokes/random?escape=javascript&firstName=' + name[0] + '&lastName=' + name[1], function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           return
         }
-        var joke = JSON.parse(body)
-        bot.sendMessage(msg.channel, joke.value.joke)
+        var joke = JSON.parse(body);
+        bot.sendMessage(msg.channel, joke.value.joke);
       }
-    })
+    });
   }
 }
 
 Commands.yomomma = {
   description: "I'll get a random yomomma joke for you!",
   fn: function (msg, suffix) {
-    var request = require('request')
+    var request = require('request');
     request('http://api.yomomma.info/', function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           return
         }
-        var yomomma = JSON.parse(body)
+        var yomomma = JSON.parse(body);
         if (suffix === '') {
-          bot.sendMessage(msg.channel, yomomma.joke)
+          bot.sendMessage(msg.channel, yomomma.joke);
         }
       }
-    })
+    });
   }
 }
 
@@ -120,38 +120,38 @@ Commands.advice = {
   description: "I'll give you some fantastic advice!",
   noDM: true, // Ratelimits Ratelimits Ratelimits Ratelimits
   fn: function (msg) {
-    var request = require('request')
+    var request = require('request');
     request('http://api.adviceslip.com/advice', function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API has returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API has returned an unconventional response.');
           return
         }
-        var advice = JSON.parse(body)
-        msg.reply(advice.slip.advice)
+        var advice = JSON.parse(body);
+        msg.reply(advice.slip.advice);
       }
-    })
+    });
   }
 }
 
 Commands.yesno = {
   description: 'Returns a gif displaying yes or no',
   fn: function (msg, suffix) {
-    var request = require('request')
+    var request = require('request');
     request('http://yesno.wtf/api/?force=' + suffix, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           return
         }
-        var yesNo = JSON.parse(body)
-        msg.reply(yesNo.image)
+        var yesNo = JSON.parse(body);
+        msg.reply(yesNo.image);
       }
-    })
+    });
   }
 }
 
@@ -159,54 +159,54 @@ Commands.urbandictionary = {
   description: "I'll fetch what idiots on the internet think something means",
   synonyms: ['ud', 'urban'],
   fn: function (msg, suffix) {
-    var request = require('request')
+    var request = require('request');
     request('http://api.urbandictionary.com/v0/define?term=' + suffix, function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           return
         }
-        var uD = JSON.parse(body)
+        var uD = JSON.parse(body);
         if (uD.result_type !== 'no_results') {
           var msgArray = []
-          msgArray.push('**' + uD.list[0].word + '**')
-          msgArray.push(uD.list[0].definition)
-          msgArray.push('\n```')
-          msgArray.push(uD.list[0].example)
-          msgArray.push('```')
-          bot.sendMessage(msg.channel, msgArray.join('\n'))
+          msgArray.push('**' + uD.list[0].word + '**');
+          msgArray.push(uD.list[0].definition);
+          msgArray.push('\n```');
+          msgArray.push(uD.list[0].example);
+          msgArray.push('```');
+          bot.sendMessage(msg.channel, msgArray.join('\n'));
         } else {
-          msg.reply(suffix + ":This word is so screwed up, even Urban Dictionary doesn't have it in its database")
+          msg.reply(suffix + ":This word is so screwed up, even Urban Dictionary doesn't have it in its database");
         }
       }
-    })
+    });
   }
 }
 
 Commands.fact = {
   description: "I'll give you some interesting facts!",
   fn: function (msg) {
-    var request = require('request')
-    var xml2js = require('xml2js')
+    var request = require('request');
+    var xml2js = require('xml2js');
     request('http://www.fayd.org/api/fact.xml', function (error, response, body) {
       if (error) {
-        Logger.error(error)
+        Logger.error(error);
       }
       if (!error && response.statusCode === 200) {
         xml2js.parseString(body, function (err, result) {
           if (err) {
-            Logger.error(err)
+            Logger.error(err);
           }
           try {
-            msg.reply(result.facts.fact[0])
+            msg.reply(result.facts.fact[0]);
           } catch (e) {
-            bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+            bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           }
-        })
+        });
       }
-    })
+    });
   }
 }
 
@@ -219,19 +219,19 @@ Commands.dice = {
     } else {
       dice = 'd6'
     }
-    var request = require('request')
+    var request = require('request');
     request('https://rolz.org/api/?' + dice + '.json', function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           return
         }
-        var roll = JSON.parse(body)
-        msg.reply('Your ' + roll.input + ' resulted in ' + roll.result + ' ' + roll.details)
+        var roll = JSON.parse(body);
+        msg.reply('Your ' + roll.input + ' resulted in ' + roll.result + ' ' + roll.details);
       }
-    })
+    });
   }
 }
 
@@ -239,42 +239,42 @@ Commands.fancyinsult = {
   description: "I'll insult your friends!",
   synonyms: ['insult'],
   fn: function (msg, suffix) {
-    var request = require('request')
+    var request = require('request');
     request('http://quandyfactory.com/insult/json/', function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
           return
         }
-        var fancyinsult = JSON.parse(body)
+        var fancyinsult = JSON.parse(body);
         if (suffix === '') {
-          bot.sendMessage(msg.channel, fancyinsult.insult)
+          bot.sendMessage(msg.channel, fancyinsult.insult);
         } else {
-          bot.sendMessage(msg.channel, suffix + ', ' + fancyinsult.insult)
+          bot.sendMessage(msg.channel, suffix + ', ' + fancyinsult.insult);
         }
       }
-    })
+    });
   }
 }
 
 Commands.catfacts = {
   description: "I'll give you some interesting catfacts",
   fn: function (msg) {
-    var request = require('request')
+    var request = require('request');
     request('http://catfacts-api.appspot.com/api/facts', function (error, response, body) {
       if (!error && response.statusCode === 200) {
         try {
-          JSON.parse(body)
+          JSON.parse(body);
         } catch (e) {
-          bot.sendMessage(msg.channel, 'The API returned an unconventional response')
+          bot.sendMessage(msg.channel, 'The API returned an unconventional response');
           return
         }
-        var catFact = JSON.parse(body)
-        msg.reply(catFact.facts[0])
+        var catFact = JSON.parse(body);
+        msg.reply(catFact.facts[0]);
       }
-    })
+    });
   }
 }
 
@@ -283,28 +283,28 @@ Commands.e621 = {
   usage: '<tags> multiword tags need to be typed like: wildbeast_is_a_discord_bot',
   nsfw: true,
   fn: function (msg, suffix) {
-    msg.channel.sendTyping()
-    unirest.post(`https://e621.net/post/index.json?limit=30&tags=${suffix}`)
+    msg.channel.sendTyping();
+    unirest.post(`https://e621.net/post/index.json?limit=30&tags=${suffix}`);
         .headers({
           'Accept': 'application/json',
           'User-Agent': 'Unirest Node.js'
-        })
+        });
         // Fetching 30 posts from E621 with the given tags
         .end(function (result) {
           if (result.body.length < 1) {
             msg.reply('Sorry, nothing found.') // Correct me if it's wrong.
           } else {
-            var count = Math.floor((Math.random() * result.body.length))
+            var count = Math.floor((Math.random() * result.body.length));
             var FurryArray = []
             if (suffix) {
-              FurryArray.push(`${msg.author.mention}, you've searched for \`${suffix}\``)
+              FurryArray.push(`${msg.author.mention}, you've searched for \`${suffix}\``);
             } else {
-              FurryArray.push(`${msg.author.mention}, you've searched for \`random\``)
+              FurryArray.push(`${msg.author.mention}, you've searched for \`random\``);
             } // hehe no privacy if you do the nsfw commands now.
-            FurryArray.push(result.body[count].file_url)
-            bot.sendMessage(msg.channel, FurryArray.join('\n'))
+            FurryArray.push(result.body[count].file_url);
+            bot.sendMessage(msg.channel, FurryArray.join('\n'));
           }
-        })
+        });
   }
 }
 
@@ -312,30 +312,30 @@ Commands.rule34 = {
   description: 'Rule#34 : If it exists there is porn of it. If not, start uploading.',
   nsfw: true,
   fn: function (msg, suffix) {
-    msg.channel.sendTyping()
+    msg.channel.sendTyping();
     unirest.post('http://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + suffix) // Fetching 100 rule34 pics
       .end(function (result) {
-        var xml2js = require('xml2js')
+        var xml2js = require('xml2js');
         if (result.body.length < 75) {
           msg.reply('sorry, nothing found.') // Correct me if it's wrong.
         } else {
           xml2js.parseString(result.body, (err, reply) => {
             if (err) {
-              bot.sendMessage(msg.channel, 'The API returned an unconventional response.')
+              bot.sendMessage(msg.channel, 'The API returned an unconventional response.');
             } else {
-              var count = Math.floor((Math.random() * reply.posts.post.length))
+              var count = Math.floor((Math.random() * reply.posts.post.length));
               var FurryArray = []
               if (!suffix) {
-                FurryArray.push(msg.author.mention + ", you've searched for `random`")
+                FurryArray.push(msg.author.mention + ", you've searched for `random`");
               } else {
-                FurryArray.push(msg.author.mention + ", you've searched for `" + suffix + '`')
+                FurryArray.push(msg.author.mention + ", you've searched for `" + suffix + '`');
               }
-              FurryArray.push('http:' + reply.posts.post[count].$.file_url)
-              bot.sendMessage(msg.channel, FurryArray.join('\n'))
+              FurryArray.push('http:' + reply.posts.post[count].$.file_url);
+              bot.sendMessage(msg.channel, FurryArray.join('\n'));
             }
-          })
+          });
         }
-      })
+      });
   }
 }
 
@@ -343,27 +343,27 @@ Commands.meme = {
   description: "I'll create a meme with your suffixes!",
   usage: '<memetype> "<Upper line>" "<Bottom line>" **Quotes are important!**',
   fn: function (msg, suffix, bot) {
-    var tags = suffix.split('"')
+    var tags = suffix.split('"');
     var memetype = tags[0].split(' ')[0]
-    var meme = require('./memes.json')
-    var Imgflipper = require('imgflipper')
-    var imgflipper = new Imgflipper(config.api_keys.imgflip.username, config.api_keys.imgflip.password)
+    var meme = require('./memes.json');
+    var Imgflipper = require('imgflipper');
+    var imgflipper = new Imgflipper(config.api_keys.imgflip.username, config.api_keys.imgflip.password);
     imgflipper.generateMeme(meme[memetype], tags[1] ? tags[1] : '', tags[3] ? tags[3] : '', (err, image) => {
       if (err) {
-        msg.reply('Please try again.')
+        msg.reply('Please try again.');
       } else {
         var guild = msg.guild
         var user = bot.User
-        var guildPerms = user.permissionsFor(guild)
+        var guildPerms = user.permissionsFor(guild);
         if (guildPerms.Text.MANAGE_MESSAGES) {
-          msg.delete()
-          msg.reply(image)
+          msg.delete();
+          msg.reply(image);
         } else {
-          msg.reply(image)
-          bot.sendMessage(msg.channel, '*This works best when I have the permission to delete messages!*')
+          msg.reply(image);
+          bot.sendMessage(msg.channel, '*This works best when I have the permission to delete messages!*');
         }
       }
-    })
+    });
   }
 }
 
